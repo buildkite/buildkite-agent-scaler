79e28342198ce400d987eb5d79d98c3cd64e74f2
Lachlan Donald <lachlan@buildkite.com>
2018-05-25 15:01:35 +1000

    Build lambda in docker

--- .buildkite/steps/build-lambda.sh
+++ .buildkite/steps/build-lambda.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 set -eux
-make build
-buildkite-agent artifact upload handler.zip
+
+docker run --rm \
+  -e HANDLER=handler \
+  -e PACKAGE=handler \
+  -e GOPATH=/go \
+  -e LDFLAGS='' \
+  -v "$PWD:/go/src/github.com/buildkite/buildkite-agent-scaler" \
+  -w /go/src/github.com/buildkite/buildkite-agent-scaler \
+  golang:1.10 make all
+
+# buildkite-agent artifact upload handler.zip
