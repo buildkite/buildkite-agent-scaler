389be80b4001638c762b2f69b5fcdc71b5534734
Keith Duncan <keith_duncan@me.com>
2021-06-09 08:36:39 +1000

    Log in the calculator

--- scaler/scaling_calculator.go
+++ scaler/scaling_calculator.go
@@ -33,6 +33,8 @@ func (sc *ScalingCalculator) DesiredCount(metrics *buildkite.AgentMetrics, asg *
 		desired = sc.perInstance(agentsRequired)
 	}
 
+	log.Printf("🧮 Agents required %d, Instances required %d", agentsRequired, desired)
+
 	// If there are less agents registered than we'd expect based on the size
 	// of the autoscaling group, then there may be instances not accepting
 	// jobs: possibly because they are in the process of draining jobs for a
@@ -46,8 +48,13 @@ func (sc *ScalingCalculator) DesiredCount(metrics *buildkite.AgentMetrics, asg *
 	// instances, this should cover the worst case scenario(s) where there may
 	// not be any instances accepting jobs.
 	if metrics.TotalAgents < int64(sc.agentsPerInstance) * 2 {
+		log.Printf("🧮 Fewer than 2 instances worth of agents running")
+
 		anticipated := (asg.DesiredCount - asg.Pending) * int64(sc.agentsPerInstance)
 		shortfall := anticipated - metrics.TotalAgents
+
+		log.Printf("🧮 Expected agents %d, shortfall %d", anticipated, shortfall)
+
 		if shortfall > 0 {
 			desired += sc.perInstance(shortfall)
 		}
