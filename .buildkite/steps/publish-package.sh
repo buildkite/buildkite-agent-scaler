05908c6b0dcba1d6537bc94ec77334dc840e0b39
Narthana Epa <narthana.epa@gmail.com>
2022-11-30 15:23:53 +1100

    Publish edge SAR instead

--- .buildkite/steps/publish-package.sh
+++ .buildkite/steps/publish-package.sh
@@ -7,11 +7,11 @@ VERSION=$(buildkite-agent meta-data get version)
 buildkite-agent artifact download 'packaged.yml' .
 
 if [[ -z "${BUILDKITE_TAG:-}" ]]; then
-  aws s3 cp \
-    --acl public-read \
-    packaged.yml \
-    "s3://buildkite-serverless-apps-us-east-1/packaged/elastic-ci/agent-scaler/$VERSION/packaged.yml"
-
+  aws serverlessrepo create-application-version \
+    --application-id arn:aws:serverlessrepo:us-east-1:172840064832:applications/buildkite-agent-scaler-edge \
+    --template-body file://packaged.yml \
+    --semantic-version "$VERSION" \
+    --source-code-url "https://github.com/buildkite/buildkite-agent-scaler/tree/$(git rev-parse HEAD)/"
 else
   aws serverlessrepo create-application-version \
     --application-id arn:aws:serverlessrepo:us-east-1:172840064832:applications/buildkite-agent-scaler \
