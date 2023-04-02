47c6fedfbe964b22988e100a7964a991bf153a40
Narthana Epa <narthana.epa@gmail.com>
2023-04-03 08:11:15 +1000

    Fix exporting from command substitution

--- .buildkite/steps/github-release.sh
+++ .buildkite/steps/github-release.sh
@@ -2,7 +2,14 @@
 set -e
 
 echo '--- Getting credentials from SSM'
-export GITHUB_RELEASE_ACCESS_TOKEN=$(aws ssm get-parameter --name /pipelines/buildkite-agent-scaler/GITHUB_RELEASE_ACCESS_TOKEN --with-decryption --output text --query Parameter.Value --region us-east-1)
+GITHUB_RELEASE_ACCESS_TOKEN=$( \
+  aws ssm get-parameter \
+    --name /pipelines/buildkite-agent-scaler/GITHUB_RELEASE_ACCESS_TOKEN \
+    --with-decryption \
+    --output text \
+    --query Parameter.Value \
+    --region us-east-1 \
+)
 
 if [[ "$GITHUB_RELEASE_ACCESS_TOKEN" == "" ]]; then
   echo "Error: Missing \$GITHUB_RELEASE_ACCESS_TOKEN"
@@ -13,6 +20,7 @@ VERSION=$(buildkite-agent meta-data get "version")
 buildkite-agent artifact download "handler.zip" .
 
 echo "--- 🚀 Releasing $VERSION"
+export GITHUB_RELEASE_ACCESS_TOKEN
 github-release "v$VERSION" handler.zip \
   --commit "$(git rev-parse HEAD)" \
   --github-repository "buildkite/buildkite-agent-scaler"
