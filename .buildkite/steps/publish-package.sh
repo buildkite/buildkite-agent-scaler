3d91ca27dfbea108b425c915eab53d710a7965ea
Narthana Epa <narthana.epa@gmail.com>
2023-08-17 15:43:09 +1000

    Apply RELEASE_DRY_RUN to publish-package step

--- .buildkite/steps/publish-package.sh
+++ .buildkite/steps/publish-package.sh
@@ -2,18 +2,20 @@
 
 set -eufo pipefail
 
+source .buildkite/lib/release-dry-run.sh
+
 VERSION=$(buildkite-agent meta-data get version)
 
 buildkite-agent artifact download packaged.yml .
 
-if [[ -z $BUILDKITE_TAG ]]; then
+if [[ -z "${BUILDKITE_TAG:-}" ]]; then
   APP_ID=arn:aws:serverlessrepo:us-east-1:172840064832:applications/buildkite-agent-scaler-edge
 else
   APP_ID=arn:aws:serverlessrepo:us-east-1:172840064832:applications/buildkite-agent-scaler
 fi
 
 echo --- ":aws::lambda: Publishing version $VERSION to SAR"
-aws serverlessrepo create-application-version \
+release_dry_run aws serverlessrepo create-application-version \
   --application-id "$APP_ID" \
   --template-body file://packaged.yml \
   --semantic-version "${VERSION#v}" \
