5d58e3966dffd7729e069d346f6d07c1b88ff226
Łukasz Jendrysik <lukasz@buildkite.com>
2025-06-16 16:27:14 +0200

    feat: update tooling to use mise bootstrap mise feat: added mise setup to pipeline and steps scripts Update development docs

--- .buildkite/steps/tests.sh
+++ .buildkite/steps/tests.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -euo pipefail
 
-go install gotest.tools/gotestsum@v1.12.0
-
 echo '+++ Running tests'
-gotestsum --junitfile "junit-${OSTYPE}.xml" -- -count=1 -failfast ./...
+./bin/mise exec -- gotestsum --junitfile "junit-${OSTYPE}.xml" -- -count=1 -failfast ./...
